779829c8463792544fd383751c8451410e878394
vcamargo <vcamargo.e@gmail.com>
2018-04-26 20:11:12 -0500

    update 26

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var morgan = require('morgan')
+var mongoose = require('mongoose')
+
 var app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan('common'));
 
-var arduino = require('./routes/arduino')
+// --- Mongo Config ---
+mongoose.Promise = global.Promise;
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost:27017/sda");
 
-app.use('/arduino', arduino);
+
+// var arduino = require('./routes/arduino')
+// app.use('/arduino', arduino);
+
+var users = require('./routes/users')
+app.use('/users', users)
 
 app.get('/', function (req, res) {
-  res.send(req.ejemplo)
+  res.send({
+    message : "Hello World"
+  })
 })
 
 module.exports = app;
